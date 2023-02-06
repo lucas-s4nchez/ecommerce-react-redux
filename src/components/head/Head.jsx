ef335bb68e348ff4e32ac7f5c0dc16490cc8790f
Lucas <lucassanchezdl@gmail.com>
2023-02-06 09:38:40 -0300

    edit head component

--- src/components/head/Head.jsx
+++ src/components/head/Head.jsx
@@ -11,13 +11,16 @@ export const Head = ({ title, description, url, images }) => {
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={images} />
+      <meta property="og:image" content={"../public/mercado-pago-icon.png"} />
       {/* Twitter  */}
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={url} />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
-      <meta property="twitter:image" content={images} />
+      <meta
+        property="twitter:image"
+        content={"../public/mercado-pago-icon.png"}
+      />
     </Helmet>
   );
 };
